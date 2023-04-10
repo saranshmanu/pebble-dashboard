6fdd07412dd34d8484c336d2fca7f81194a98d9e
Saransh Mittal <saranshmanu@yahoo.co.in>
2023-04-10 22:23:29 +0530

    Adds general settings sub page

--- src/components/settings/General.js
+++ src/components/settings/General.js
@@ -1,3 +1,50 @@
-const General = () => <></>;
+import { Button, Divider } from "antd";
+import {
+  UsergroupAddOutlined,
+  FireOutlined,
+  CloseCircleOutlined,
+  ExportOutlined,
+  FolderAddOutlined,
+  FilePdfOutlined,
+} from "@ant-design/icons";
+
+const General = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <Button type="link" size="large" icon={<FolderAddOutlined />}>
+          Import Holdings
+        </Button>
+      </div>
+      <div>
+        <Button type="link" size="large" icon={<UsergroupAddOutlined />}>
+          Create Institution
+        </Button>
+      </div>
+      <div>
+        <Button type="link" size="large" icon={<FireOutlined />}>
+          Format Cache
+        </Button>
+      </div>
+      <div>
+        <Button type="link" size="large" icon={<FilePdfOutlined />}>
+          Generate Holding Report
+        </Button>
+      </div>
+      <div>
+        <Button type="link" size="large" icon={<ExportOutlined />}>
+          Export Data (in JSON format)
+        </Button>
+      </div>
+      <Divider />
+      <div>
+        <Button type="link" size="large" icon={<CloseCircleOutlined />} danger>
+          Close Application
+        </Button>
+      </div>
+      <Divider />
+    </div>
+  );
+};
 
 export default General;
